ea59211de2f001929e9fd6d482ca318cfbc3e65f
yt17 <tyemek@gmail.com>
2022-03-01 23:45:03 +0300

    asd
    asd

---
+++ ApiWithJWT/CORE/DepencyResolver/CoreModule.cs
@@ -0,0 +1,16 @@
+﻿using CORE.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CORE.DepencyResolver
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection serviceProvider)
+        {
+            serviceProvider.AddMemoryCache();
+        }
+    }
+}
---
+++ ApiWithJWT/CORE/Utilities/ServiceTool.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CORE.Utilities
+{
+    public static class ServiceTool
+    {
+        public static IServiceProvider serviceProvider { get; set; }
+        public static IServiceCollection Create(IServiceCollection service)
+        {
+            serviceProvider = service.BuildServiceProvider();
+            return service;
+        }
+    }
+}
