621e59635fb83ec76413b13f7c6258d61b788f20
yt17 <tyemek@gmail.com>
2022-03-04 00:46:36 +0300

    new codes
    new codes

---
+++ ApiWithJWT/CORE/CrossCuttingConcerns/MemoryCacheManager.cs
@@ -0,0 +1,47 @@
+﻿using CORE.Utilities;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CORE.CrossCuttingConcerns
+{
+    public class MemoryCacheManager : ICacheManager 
+    {
+        private IMemoryCache cache;
+        public MemoryCacheManager()
+        {
+            cache=ServiceTool.serviceProvider.GetService<IMemoryCache>();
+        }
+        public void Add(string key, object data, int duration)
+        {
+            cache.Set(key, TimeSpan.FromMilliseconds(duration));
+        }
+
+        public T Get<T>(string key)
+        {
+            return cache.Get<T> (key);
+        }
+
+        public object Get(string key)
+        {
+            return cache.Get(key);
+        }
+
+        public bool IsAdd(string key)
+        {
+            return cache.TryGetValue(key, out _);
+        }
+
+        public void Remove(string key)
+        {
+            cache.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+           // cache.
+        }
+    }
+}
